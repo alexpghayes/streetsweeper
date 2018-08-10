bf64c437c167e88c703a679ace35e43e0123595f
Alex Hayes <alexpghayes@gmail.com>
2018-08-10 15:34:16 -0700

    Minor tweaks

--- R/hello.R
+++ R/hello.R
@@ -5,7 +5,7 @@
 #' @param object
 #'
 #' @return asdf
-#' @importFrom broom tidy
+#' @import broom
 #' @export
 #'
 use_tidy_method_from_broom <- function(object) {
@@ -17,10 +17,9 @@ use_tidy_method_from_broom <- function(object) {
 #'
 #' @param object
 #'
-#' @importFrom broom tidy
+#' @import broom
 #' @import broom.mixed
 #' @export
 use_tidy_method_from_broom.mixed <- function(object) {
-  # broom.mixed::tidy(object)  # doesn't work since broom.mixed doesn't export `tidy()`
-  tidy(object)
+  broom.mixed::tidy(object)  # doesn't work since broom.mixed doesn't export `tidy()`
 }
